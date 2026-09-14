25854680118e5b234c06d3a93ebe9e6a1c733809
mel <amelissariver@gmail.com>
2026-09-13 21:17:31 -0300

    feat(config): add ui-config block and kof.config functions

--- kof-compiler/src/main/java/dev/kof/compiler/js/JsRuntimeSlices.java
+++ kof-compiler/src/main/java/dev/kof/compiler/js/JsRuntimeSlices.java
@@ -27,6 +27,7 @@ final class JsRuntimeSlices {
             new Block("ui-layout", JsRuntimeUiLayout.UI_LAYOUT_RUNTIME, false),
             new Block("ui-jsonmap", JsRuntimeUiJsonMap.JSON_MAP_RUNTIME, false),
             new Block("ui-web", JsRuntimeUiWeb.UI_WEB_RUNTIME, false),
+            new Block("ui-config", JsRuntimeUiConfig.CONFIG_RUNTIME, false),
             new Block("ui-support", JsRuntimeUiSupport.UI_SUPPORT_RUNTIME, false),
             new Block("security", JsRuntimeUiSecurity.UI_SECURITY_RUNTIME, false),
             new Block("crypto", JsRuntimeUiCrypto.UI_CRYPTO_RUNTIME, false),
